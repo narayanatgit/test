693bc3f6b7960491e7cc08b5e5379152d6fc0ee3
narayanatgit <124003158@sastra.ac.in>
2023-08-09 15:48:30 +0530

    first commit

--- server.js
+++ server.js
@@ -1,21 +1,47 @@
 const express = require('express');
+const body=require('body-parser')
 const app = express();
-
+app.use(body.json());
+app.use(express.json())
+app.use(body.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 8080;
-
+var i=0;
 // Greeting 
+var a=[]
 app.get('/greeting', (req, res) => {
     return res.send('Hello world!');
 });
 
 // Register Employee
 app.post('/employee', (req, res) => {
-    return res.send();
-});
+    console.log(req.body)
+    a.push({  id:++i,
+        name:req.body.name,
+        city:req.body.city})
+        console.log(a)
+    return res.send({
+        id:i,
+       name:req.body.name,
+       city:req.body.city
 
+    });
+});
+console.log(a)
 // Get Employee details
 app.get('/employee/:id', (req, res) => {
-    return res.send();
+        id=req.params
+   data=a.map((m)=>
+   {
+      if(m.id==id)
+      {
+        return m
+      }
+   })
+    return res.send( 
+               {
+                  content:data
+               }
+    );
 });
 
 // Get all Employee details
